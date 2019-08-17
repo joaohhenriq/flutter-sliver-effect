acd467ee89b651000610c18bbbeafd99c0d9fef6
joaoh <joao_camargo10@hotmail.com>
2019-08-17 12:44:10 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Flutter Sliver Effect
+A simple app to practice CustomScrollView widget.
+
+<img src="static/1.jpg" width="200"> <img src="static/2.jpg" width="200"> <img src="static/3.jpg" width="200"> <img src="static/4.jpg" width="200">
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            leading: Icon(Icons.arrow_back),
+            backgroundColor: Colors.blue[300],
+            forceElevated: true,
+            floating: true,
+            snap: true,
+            centerTitle: true,
+            expandedHeight: 250,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text("Sliver effect", style: TextStyle(
+                fontFamily: 'LibreCaslon', fontSize: 24
+              ),),
+              centerTitle: true,
+              background: Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  Image.network(
+                    "https://media.wired.com/photos/598e35fb99d76447c4eb1f28/master/pass/phonepicutres-TA.jpg", fit: BoxFit.cover,
+                  ),
+                  DecoratedBox(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                        Colors.transparent,
+                        Colors.transparent,
+                        Colors.transparent,
+                        Colors.black
+                      ], begin: Alignment.topCenter, end: Alignment.bottomCenter)
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+                (context, index){
+                  return Card(
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          CircleAvatar(
+                            backgroundColor: Colors.transparent,
+                            backgroundImage: NetworkImage('https://cdn.pixabay.com/photo/2014/12/15/17/16/pier-569314__340.jpg'),
+                          ),
+                          SizedBox(width: 20,),
+                          Text("Card content :D"),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
